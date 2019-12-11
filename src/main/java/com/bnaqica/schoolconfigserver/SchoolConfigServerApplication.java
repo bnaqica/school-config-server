e4a202044426aa9ed1d26017509688f696b0e836
Brian Naqica <brian.naqica@davita.com>
2019-12-11 09:28:13 -0700

    Initial config server commit

---
+++ src/main/java/com/bnaqica/schoolconfigserver/SchoolConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.bnaqica.schoolconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolConfigServerApplication.class, args);
+	}
+
+}
